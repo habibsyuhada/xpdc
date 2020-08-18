420136035fdaba176d575123c7eeb70ddfa2fdde
Habib Syuhada <habibsumedang@gmail.com>
2020-08-19 06:10:39 +0700

    db remote

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'xpdc',
+	'hostname' => 'remotemysql.com',
+	'username' => 'eHxnBLJAYf',
+	'password' => 'DUwPsP5Zir',
+	'database' => 'eHxnBLJAYf',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,

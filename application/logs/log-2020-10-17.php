a91bf24ed9a1b10b7ceaeff490c411d10cbe5ced
Habib Syuhada <habibsumedang@gmail.com>
2020-10-19 15:50:08 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ application/logs/log-2020-10-17.php
@@ -0,0 +1,50 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-10-17 08:30:56 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2020-10-17 08:31:01 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2020-10-17 08:50:40 --> Severity: Notice --> Undefined variable: country C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 134
+ERROR - 2020-10-17 08:50:40 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 134
+ERROR - 2020-10-17 08:50:40 --> Severity: Notice --> Undefined variable: country C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 174
+ERROR - 2020-10-17 08:50:40 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 174
+ERROR - 2020-10-17 08:50:40 --> Severity: Notice --> Undefined variable: country C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 258
+ERROR - 2020-10-17 08:50:40 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 258
+ERROR - 2020-10-17 08:54:29 --> Severity: Notice --> Undefined index: status_pickup C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 370
+ERROR - 2020-10-17 08:54:29 --> Severity: Notice --> Undefined index: status_pickup C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 371
+ERROR - 2020-10-17 09:42:49 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2020-10-17 10:08:56 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2020-10-17 10:14:30 --> Severity: Notice --> Undefined index:  C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 480
+ERROR - 2020-10-17 10:14:30 --> Severity: Notice --> Undefined index:  C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 481
+ERROR - 2020-10-17 10:14:30 --> Severity: Notice --> Undefined index:  C:\xampp\htdocs\xpdc\application\views\shipment\shipment_update.php 482
+ERROR - 2020-10-17 10:15:36 --> Severity: Notice --> Undefined index: sea C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 274
+ERROR - 2020-10-17 10:15:36 --> Severity: Notice --> Undefined index: description C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 325
+ERROR - 2020-10-17 10:15:36 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 325
+ERROR - 2020-10-17 10:23:44 --> Severity: Notice --> Undefined index: sea C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 274
+ERROR - 2020-10-17 10:23:44 --> Severity: Notice --> Undefined index: description C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 329
+ERROR - 2020-10-17 10:23:44 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 329
+ERROR - 2020-10-17 10:24:34 --> Severity: Notice --> Undefined index: sea C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 274
+ERROR - 2020-10-17 10:25:42 --> Severity: Notice --> Undefined index: sea C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 274
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:49 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:50 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:51 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:12:53 --> 404 Page Not Found: Assets/landing_page
+ERROR - 2020-10-17 15:12:53 --> 404 Page Not Found: Assets/landing_page
+ERROR - 2020-10-17 15:12:53 --> 404 Page Not Found: Assets/landing_page
+ERROR - 2020-10-17 15:16:38 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:16:41 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:16:42 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:16:42 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:16:42 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:16:42 --> 404 Page Not Found: Assets/images
+ERROR - 2020-10-17 15:16:42 --> 404 Page Not Found: Assets/images

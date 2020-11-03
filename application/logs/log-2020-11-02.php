fb97437daf1677660f270cd5e279b26424008b0b
Habib Syuhada <habibsumedang@gmail.com>
2020-11-03 08:31:50 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/logs/log-2020-11-02.php
+++ application/logs/log-2020-11-02.php
@@ -1,7 +1,20 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+<<<<<<< Updated upstream
 ERROR - 2020-11-02 20:26:11 --> 404 Page Not Found: Assets/favicon.ico
 ERROR - 2020-11-02 20:27:05 --> 404 Page Not Found: Home/favicon.ico
 ERROR - 2020-11-02 20:27:21 --> 404 Page Not Found: Shipment/favicon.ico
 ERROR - 2020-11-02 20:28:41 --> Severity: Notice --> Undefined variable: value C:\xampp\htdocs\xpdc\application\views\shipment\shipment_cost.php 40
 ERROR - 2020-11-02 20:31:50 --> 404 Page Not Found: Master_tracking/favicon.ico
+=======
+ERROR - 2020-11-02 09:00:47 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-02 09:00:47 --> Unable to connect to the database
+ERROR - 2020-11-02 09:20:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-02 09:20:05 --> Unable to connect to the database
+ERROR - 2020-11-02 09:20:26 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2020-11-02 09:23:20 --> 404 Page Not Found: Home/favicon.ico
+ERROR - 2020-11-02 09:23:23 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2020-11-02 09:23:51 --> 404 Page Not Found: Master_tracking/favicon.ico
+>>>>>>> Stashed changes

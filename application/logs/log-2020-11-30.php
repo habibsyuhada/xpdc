cefc9dc974dd8405f00b2348f7c27f765f931503
Habib Syuhada <habibsumedang@gmail.com>
2020-12-01 09:25:34 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/logs/log-2020-11-30.php
+++ application/logs/log-2020-11-30.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+<<<<<<< Updated upstream
 ERROR - 2020-11-30 05:50:03 --> 404 Page Not Found: Assets/favicon.ico
 ERROR - 2020-11-30 05:50:13 --> 404 Page Not Found: Home/favicon.ico
 ERROR - 2020-11-30 05:50:37 --> 404 Page Not Found: Shipment/favicon.ico
@@ -21,3 +22,45 @@ ERROR - 2020-11-30 06:05:10 --> Severity: Notice --> Undefined variable: shipmen
 ERROR - 2020-11-30 06:05:10 --> Severity: Notice --> Undefined variable: shipment C:\xampp\htdocs\xpdc\application\views\shipment\shipment_bill.php 135
 ERROR - 2020-11-30 06:05:10 --> Severity: Notice --> Undefined variable: shipment C:\xampp\htdocs\xpdc\application\views\shipment\shipment_bill.php 143
 ERROR - 2020-11-30 06:05:10 --> Severity: Notice --> Undefined variable: shipment C:\xampp\htdocs\xpdc\application\views\shipment\shipment_bill.php 143
+=======
+ERROR - 2020-11-30 08:15:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 08:15:28 --> Unable to connect to the database
+ERROR - 2020-11-30 08:44:19 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 08:44:19 --> Unable to connect to the database
+ERROR - 2020-11-30 09:08:59 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:08:59 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:08:59 --> Unable to connect to the database
+ERROR - 2020-11-30 09:08:59 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:13 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:16 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:54 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:54 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:56 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:56 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:57 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:57 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:57 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:57 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:57 --> Unable to connect to the database
+ERROR - 2020-11-30 09:32:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u6129815_xpdc'@'localhost' (using password: YES) C:\xampp\htdocs\xpdc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-11-30 09:32:58 --> Unable to connect to the database
+ERROR - 2020-11-30 09:33:11 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2020-11-30 09:33:28 --> 404 Page Not Found: Home/favicon.ico
+ERROR - 2020-11-30 09:33:35 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2020-11-30 17:04:25 --> 404 Page Not Found: Report/favicon.ico
+>>>>>>> Stashed changes

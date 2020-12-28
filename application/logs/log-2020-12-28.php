3dc5f1961317bfa526485b8a70660452914f41ef
Habib Syuhada <habibsumedang@gmail.com>
2020-12-28 11:23:13 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ application/logs/log-2020-12-28.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-12-28 10:11:51 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2020-12-28 10:11:59 --> 404 Page Not Found: Home/favicon.ico
+ERROR - 2020-12-28 10:12:13 --> 404 Page Not Found: Quotation/favicon.ico
+ERROR - 2020-12-28 10:12:23 --> 404 Page Not Found: Payment_terms/favicon.ico
+ERROR - 2020-12-28 10:12:46 --> Severity: Notice --> Undefined variable: where C:\xampp\htdocs\xpdc\application\controllers\Commercial.php 40
+ERROR - 2020-12-28 10:12:51 --> 404 Page Not Found: Commercial/favicon.ico
+ERROR - 2020-12-28 10:18:43 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2020-12-28 11:06:25 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-28 11:06:25 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-28 11:15:54 --> Severity: error --> Exception: Call to undefined method Home_mod::uom_list() C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 746
+ERROR - 2020-12-28 11:15:57 --> Severity: error --> Exception: Call to undefined method Home_mod::uom_list() C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 872
+ERROR - 2020-12-28 11:15:59 --> Severity: error --> Exception: Call to undefined method Home_mod::uom_list() C:\xampp\htdocs\xpdc\application\controllers\Quotation.php 183
+ERROR - 2020-12-28 11:16:01 --> Severity: error --> Exception: Call to undefined method Home_mod::uom_list() C:\xampp\htdocs\xpdc\application\controllers\Quotation.php 49
+ERROR - 2020-12-28 11:16:27 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-28 11:16:27 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-28 11:18:33 --> 404 Page Not Found: Uom/favicon.ico
+ERROR - 2020-12-28 11:18:41 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-28 11:18:41 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28

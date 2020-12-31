0ca2f5260a0a407729fa1017fccd3fd0fd543cff
Habib Syuhada <habibsumedang@gmail.com>
2020-12-31 08:13:28 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/logs/log-2020-12-29.php
+++ application/logs/log-2020-12-29.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+<<<<<<< Updated upstream
 ERROR - 2020-12-29 20:06:51 --> 404 Page Not Found: Assets/favicon.ico
 ERROR - 2020-12-29 20:07:03 --> 404 Page Not Found: Home/favicon.ico
 ERROR - 2020-12-29 20:07:11 --> 404 Page Not Found: Quotation/favicon.ico
@@ -10,3 +11,120 @@ ERROR - 2020-12-29 20:12:25 --> Severity: Warning --> Invalid argument supplied 
 ERROR - 2020-12-29 20:13:44 --> 404 Page Not Found: Shipment/favicon.ico
 ERROR - 2020-12-29 20:16:20 --> 404 Page Not Found: Zone/favicon.ico
 ERROR - 2020-12-29 20:19:04 --> 404 Page Not Found: Branch/favicon.ico
+=======
+ERROR - 2020-12-29 08:10:02 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2020-12-29 08:10:10 --> 404 Page Not Found: Home/favicon.ico
+ERROR - 2020-12-29 08:10:14 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-29 08:10:14 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 28
+ERROR - 2020-12-29 08:10:14 --> 404 Page Not Found: Quotation/favicon.ico
+ERROR - 2020-12-29 08:20:22 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:20:22 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:32:01 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:32:01 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:32:24 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:32:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:32:30 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:32:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:34:06 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:34:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:34:20 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:34:20 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:38:03 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:38:03 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:40:38 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:40:38 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:44:03 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:44:03 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:44:52 --> Severity: Notice --> Undefined index: type_of_mode C:\xampp\htdocs\xpdc\application\views\shipment\shipment_create.php 64
+ERROR - 2020-12-29 08:44:52 --> Severity: Notice --> Undefined index: type_of_mode C:\xampp\htdocs\xpdc\application\views\shipment\shipment_create.php 72
+ERROR - 2020-12-29 08:44:54 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:44:55 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:45:10 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:45:10 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:45:43 --> Severity: Notice --> Undefined variable: shipment C:\xampp\htdocs\xpdc\application\views\shipment\shipment_create.php 66
+ERROR - 2020-12-29 08:45:43 --> Severity: Notice --> Undefined variable: shipment C:\xampp\htdocs\xpdc\application\views\shipment\shipment_create.php 74
+ERROR - 2020-12-29 08:45:46 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:45:46 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:45:54 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:45:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:48:31 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:48:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:02 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:02 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:35 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:36 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:36 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:57 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:49:57 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:50:35 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:50:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:50:38 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:50:38 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:50:42 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2020-12-29 08:52:01 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:52:01 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:56:13 --> Severity: Notice --> Undefined variable: cargo_list C:\xampp\htdocs\xpdc\application\views\shipment\shipment_create.php 289
+ERROR - 2020-12-29 08:56:13 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\xpdc\application\views\shipment\shipment_create.php 289
+ERROR - 2020-12-29 08:56:16 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:56:16 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:57:28 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:57:28 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:57:35 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 08:57:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:00:06 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:00:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:00:18 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:00:18 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:07:12 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:07:12 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: container_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 212
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: container_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 213
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: seal_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 219
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: seal_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 220
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: cipl_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 259
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: cipl_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 261
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: permit_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 267
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined index: permit_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 268
+ERROR - 2020-12-29 09:07:42 --> Severity: Notice --> Undefined variable: post C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 283
+ERROR - 2020-12-29 09:08:33 --> Severity: Notice --> Undefined index: cipl_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 259
+ERROR - 2020-12-29 09:08:33 --> Severity: Notice --> Undefined index: cipl_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 261
+ERROR - 2020-12-29 09:08:33 --> Severity: Notice --> Undefined index: permit_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 267
+ERROR - 2020-12-29 09:08:33 --> Severity: Notice --> Undefined index: permit_no C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 268
+ERROR - 2020-12-29 09:08:33 --> Severity: Notice --> Undefined variable: post C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 283
+ERROR - 2020-12-29 09:08:54 --> Severity: Notice --> Undefined variable: post C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 283
+ERROR - 2020-12-29 09:09:47 --> Severity: Notice --> Undefined property: Shipment::$quotation_mod C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 328
+ERROR - 2020-12-29 09:09:48 --> Severity: error --> Exception: Call to a member function quotation_update_process_db() on null C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 328
+ERROR - 2020-12-29 09:10:37 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:10:37 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:11:29 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:11:29 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:11:45 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:11:45 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:12:19 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:12:19 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:12:55 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:12:55 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:13:46 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:13:46 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:15:10 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:15:10 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 09:32:40 --> 404 Page Not Found: Template/plugins
+ERROR - 2020-12-29 09:32:41 --> 404 Page Not Found: Template/plugins
+ERROR - 2020-12-29 09:42:39 --> 404 Page Not Found: Template/plugins
+ERROR - 2020-12-29 09:42:39 --> 404 Page Not Found: Template/plugins
+ERROR - 2020-12-29 09:51:14 --> 404 Page Not Found: File/driver
+ERROR - 2020-12-29 09:51:17 --> Severity: Notice --> Undefined offset: 0 C:\xampp\htdocs\xpdc\application\controllers\Driver.php 101
+ERROR - 2020-12-29 09:59:42 --> Severity: Notice --> Undefined index: pickup_same_as C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 408
+ERROR - 2020-12-29 10:00:28 --> Severity: Notice --> Undefined index: pickup_same_as C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 408
+ERROR - 2020-12-29 10:05:23 --> Severity: Notice --> Undefined offset: 0 C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 141
+ERROR - 2020-12-29 10:05:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 286
+ERROR - 2020-12-29 10:05:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 340
+ERROR - 2020-12-29 10:07:04 --> Severity: error --> Exception: syntax error, unexpected 'public' (T_PUBLIC) C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 509
+ERROR - 2020-12-29 10:10:48 --> Severity: Notice --> Undefined offset: 0 C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 141
+ERROR - 2020-12-29 10:10:48 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 286
+ERROR - 2020-12-29 10:10:48 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 340
+ERROR - 2020-12-29 10:16:03 --> 404 Page Not Found: User/favicon.ico
+ERROR - 2020-12-29 10:16:32 --> Severity: Notice --> Undefined variable: branch_list C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+ERROR - 2020-12-29 10:16:32 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\quotation\quotation_list.php 29
+>>>>>>> Stashed changes

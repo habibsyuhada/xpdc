cca88a8ea45a17d8253b19976d73679f8bd1ba85
Habib Syuhada <habibsumedang@gmail.com>
2021-01-05 06:08:42 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ application/logs/log-2021-01-04.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-01-04 05:51:08 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2021-01-04 05:51:17 --> 404 Page Not Found: Home/favicon.ico
+ERROR - 2021-01-04 05:52:02 --> 404 Page Not Found: Shipment/favicon.ico
+ERROR - 2021-01-04 07:23:03 --> Severity: Notice --> Undefined offset: 0 C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 153
+ERROR - 2021-01-04 07:23:04 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 286
+ERROR - 2021-01-04 07:23:04 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\xpdc\application\views\shipment\shipment_receipt.php 340
+ERROR - 2021-01-04 07:23:08 --> Severity: Notice --> Undefined offset: 0 C:\xampp\htdocs\xpdc\application\controllers\Shipment.php 1149
+ERROR - 2021-01-04 07:23:08 --> Severity: Notice --> Undefined variable: value C:\xampp\htdocs\xpdc\application\views\shipment\shipment_track.php 138

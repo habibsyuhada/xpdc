35e4eb0ff50573030915736a64d6357dc5c4a01d
Habib Syuhada <habibsumedang@gmail.com>
2021-02-24 10:26:32 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/logs/log-2021-02-20.php
+++ application/logs/log-2021-02-20.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+<<<<<<< Updated upstream
 ERROR - 2021-02-20 12:51:57 --> 404 Page Not Found: Assets/favicon.ico
 ERROR - 2021-02-20 12:52:10 --> 404 Page Not Found: Assets/img
 ERROR - 2021-02-20 12:52:11 --> 404 Page Not Found: Home/favicon.ico
@@ -10,3 +11,20 @@ ERROR - 2021-02-20 12:53:59 --> 404 Page Not Found: Assets/img
 ERROR - 2021-02-20 12:53:59 --> 404 Page Not Found: Shipment/favicon.ico
 ERROR - 2021-02-20 12:57:16 --> 404 Page Not Found: Assets/img
 ERROR - 2021-02-20 12:57:21 --> 404 Page Not Found: Assets/img
+=======
+ERROR - 2021-02-20 07:36:05 --> 404 Page Not Found: Assets/favicon.ico
+ERROR - 2021-02-20 07:36:13 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:36:13 --> 404 Page Not Found: Home/favicon.ico
+ERROR - 2021-02-20 07:36:36 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:36:37 --> 404 Page Not Found: Commercial/favicon.ico
+ERROR - 2021-02-20 07:39:08 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:39:45 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:40:57 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:41:28 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:41:37 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:45:03 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:45:12 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:45:15 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:45:26 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-20 07:45:39 --> 404 Page Not Found: Assets/img
+>>>>>>> Stashed changes
